21b52601d1fb8425ded73c3c3633a31a000d8997
Jelena Gavran <jelena.gavran@fer.hr>
2022-05-11 17:31:00 +0200

    commit

--- BlueSTSDK_Python/blue_st_examples/run.py
+++ BlueSTSDK_Python/blue_st_examples/run.py
@@ -1,16 +1,16 @@
 import threading
 import example_ble_5
-import mojaproba
+import proba_tk
 #import proba as output
 import sys
 
 def main():
-	output_fun = threading.Thread(target=mojaproba.main)
-	ble = threading.Thread(target=example_ble_5.main, args=(1,))
+	output_thread = threading.Thread(target=proba_tk.main)
+	ble_thread = threading.Thread(target=example_ble_5.main, args=(1,))
 
-	output_fun.start()
-	ble.start()
-	ble.join()
+	output_thread.start()
+	ble_thread.start()
+	ble_thread.join()
 
 
 if __name__ == "__main__":
