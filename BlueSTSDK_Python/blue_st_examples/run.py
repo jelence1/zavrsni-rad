02ded93a112584b485a7b60e6a777a0b9cd4ff33
Jelena Gavran <jelena.gavran@fer.hr>
2022-05-24 22:58:35 +0200

    trying subprocesses

--- BlueSTSDK_Python/blue_st_examples/run.py
+++ BlueSTSDK_Python/blue_st_examples/run.py
@@ -26,8 +26,11 @@ def new_main():
 
 	if p.returncode == globals.EDIT_CODE:
 		print("edit audio, run audacity. ",globals.RECORDING_CODE)
-	elif p.returncode != globals.RECORDING_CODE:
-		print("Exit code: ", p.returncode)
+		sys.exit(0)
+	else: 
+		if p.returncode != globals.RECORDING_CODE:
+			print("Exit code: ", p.returncode)
+			sys.exit(0)
 
 	q = subprocess.run(["python3", "params_form.py"])
 
