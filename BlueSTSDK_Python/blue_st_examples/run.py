16902b54f3da87dd9bd08d376f3b0dfb59d9fdbf
jelence1 <jelena.gavran@fer.hr>
2022-05-31 20:23:58 +0200

    delete unused py files

--- BlueSTSDK_Python/blue_st_examples/run.py
+++ BlueSTSDK_Python/blue_st_examples/run.py
@@ -3,16 +3,9 @@ import sys
 import subprocess
 import threading
 
-from multiprocessing.pool import ThreadPool
-
-
 
 # USER IMPORTS
-import example_ble_5
-import params_form
-import recording_logic
 import globals
-import pipe
 
 
 def main():
@@ -22,16 +15,16 @@ def main():
 	p = subprocess.run(["python3", "intro_form.py"])
 
 	if p.returncode == globals.EDIT_CODE:
-		print("edit audio, run audacity. ",globals.RECORDING_CODE)
+		p = subprocess.run(["sudo", "audacity"])
+		print(p.returncode)
+		if p.returncode != 0:
+			pass
 		sys.exit(0)
 	else: 
 		if p.returncode != globals.RECORDING_CODE:
 			print("Exit code: ", p.returncode)
 			sys.exit(p.returncode)
 
-	
-	# pool = ThreadPool(processes=1)
-	# async_result = pool.apply_async(pipe.main)
 
 	p = subprocess.run(["python3", "params_form.py"], stdout=subprocess.PIPE, encoding="utf-8")
 
@@ -40,19 +33,8 @@ def main():
 		sys.exit(p.returncode)
 
 	data = p.stdout
-	print(data)
-
 	q = subprocess.run(["python3", "recording_logic.py", data])
 
-	
-def main2():
-	import output
-	output_thread = threading.Thread(target=output.main)
-	ble_thread = threading.Thread(target=example_ble_5.main, args=(1,))
-
-	output_thread.start()
-	ble_thread.start()
-	ble_thread.join()
 
 if __name__ == "__main__":
 	try:
